8caea96e18772956b2f1e0f9d785e561c1b454db
Henry Taby <henry.taby@gmail.com>
2021-03-22 06:43:09 -0400

    Proyecto seth-market

---
+++ src/main/java/com/seth/market/SethMarketApplication.java
@@ -0,0 +1,13 @@
+package com.seth.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SethMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SethMarketApplication.class, args);
+	}
+
+}
